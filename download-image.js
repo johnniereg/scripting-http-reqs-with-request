c549d6d696b4de96a5eb8e83fa417913765e46e8
Johnnie Regalado <johnniereg@gmail.com>
2017-11-06 21:11:18 +0000

    Add .on('error') and .on('response') requirements.

--- download-image.js
+++ download-image.js
@@ -1,2 +1,12 @@
 var request = require('request');
 var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')  // pick
+       .on('error', function (err) {
+          throw err;
+       })
+       .on('response', function (response) {
+          console.log(response.statusCode);
+          console.log(response.statusMessage);
+          console.log(response.headers['content-type']);
+       })
